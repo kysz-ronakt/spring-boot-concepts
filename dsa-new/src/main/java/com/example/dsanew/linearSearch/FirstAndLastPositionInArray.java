f53cda123cbefc73a08db36c47b1834eb16802ca
Ronak Thakor <ronakthakor3333@gmail.com>
2024-05-14 01:59:37 +0530

    find first and last element of target element

--- dsa-new/src/main/java/com/example/dsanew/linearSearch/FirstAndLastPositionInArray.java
+++ dsa-new/src/main/java/com/example/dsanew/linearSearch/FirstAndLastPositionInArray.java
@@ -43,6 +43,7 @@ public class FirstAndLastPositionInArray {
 
     /**
      * this function returns the index value of target
+     *
      * @param
      * @author ronak-springboot-dev
      */
@@ -56,22 +57,22 @@ public class FirstAndLastPositionInArray {
 
         int mid = start + (end - start) / 2;
 
-        if (target < arr[mid]) {
-            end = mid - 1;
-        } else if (target > arr[mid]) {
-            start = mid + 1;
-        } else {
-
-            //potential ans is found
-            ans = mid;
-
-            if (findStartIndex) {
+        while (start <= end) {
+            if (target < arr[mid]) {
                 end = mid - 1;
-            } else {
+            } else if (target > arr[mid]) {
                 start = mid + 1;
+            } else {
+                if (target == arr[mid]) {
+                    ans = mid;
+
+                    if (findStartIndex) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
             }
-
-
         }
         return ans;
     }
