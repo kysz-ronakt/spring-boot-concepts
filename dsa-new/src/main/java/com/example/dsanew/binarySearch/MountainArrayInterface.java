54e47bd85a874f11975fd9575229e2b895d3d2cd
Ronak Thakor <ronakthakor3333@gmail.com>
2024-05-17 11:24:12 +0530

    MountainArray interface added according LC

---
+++ dsa-new/src/main/java/com/example/dsanew/binarySearch/MountainArrayInterface.java
@@ -0,0 +1,8 @@
+package com.example.dsanew.binarySearch;
+
+public interface MountainArrayInterface {
+
+    int get(int index);
+
+    int length();
+}
--- dsa-new/src/main/java/com/example/dsanew/binarySearch/SearchInMountain.java
+++ dsa-new/src/main/java/com/example/dsanew/binarySearch/SearchInMountain.java
@@ -100,4 +100,70 @@ public class SearchInMountain {
         return -1;
 
     }
+
+    //====================================================================================================================================================
+    public int findInMountainArray(int target, MountainArrayInterface mountainArr) {
+        int peak = peakIndexInMountainArray(mountainArr);
+        System.out.println("peak = " + peak);
+        int firstTry = orderAgnosticBS(mountainArr, target, 0, peak);
+        System.out.println("firstTry = " + firstTry);
+
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        // Else try to search in the second half
+        return orderAgnosticBS(mountainArr, target, peak + 1, mountainArr.length() - 1);
+    }
+
+    private int peakIndexInMountainArray(MountainArrayInterface arr) {
+        int start = 0;
+        int end = arr.length() - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr.get(mid) > arr.get(mid + 1)) {
+                // Now you're in the decreasing part of the array, so this may be your answer, or look on the left side
+                // This may be our answer, and that is why it is not end = mid - 1;
+                end = mid;
+            } else {
+                // You are in the ascending part of the array
+                start = mid + 1; // Because we know that mid + 1 > mid
+            }
+        }
+        // In the end, start == end
+        // Because every time start and end are trying to find the max element for that position
+        return start;
+    }
+
+    private int orderAgnosticBS(MountainArrayInterface arr, int target, int start, int end) {
+        // Find whether the array is in ascending or descending order
+        boolean isAscending = arr.get(start) < arr.get(end);
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // Check if the target is found
+            if (arr.get(mid) == target) {
+                return mid;
+            }
+
+            if (isAscending) {
+                if (target < arr.get(mid)) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr.get(mid)) {
+                    // If your target is > mid then it will be on the left side in a descending array
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
+
